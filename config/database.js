4cb9e496498573b83a430ab9cc253cf51d336905
Maksym Sierszen <yellowsoysauce@gmail.com>
2024-09-05 22:02:41 +0200

    unimportant minor change

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('Node_Library', 'maksymsierszen', '841378125mM', {
-  host: 'localhost',
+  host: '127.0.0.1',
   dialect: 'postgres',
   port: 5432,
   logging: false
